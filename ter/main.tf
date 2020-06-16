c363d901fc539d19daefcb79bf2183595626a46c
shrutithapar <shrutithapar15@gmail.com>
2020-06-16 12:04:50 +0000

    code commit

--- ter/main.tf
+++ ter/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_key_pair" "deployer" {
   key_name = "samplekey"
-  public_key = "${file("/home/ec2-user/id_rsa.pub")}"
+  public_key = "${file("/home/ec2-user/.ssh/id_rsa.pub")}"
 }
 resource "aws_instance" "task1" {
   ami             = "${var.ami}"
